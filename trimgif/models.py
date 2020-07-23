deaab8b85093cfee7a05f4d2a989bda1c9a627b9
Om Joshi <omajoshi9@gmail.com>
2020-07-23 08:23:21 +0000

    added automatic subtitle renaming for better file organization

--- trimgif/models.py
+++ trimgif/models.py
@@ -5,7 +5,7 @@ import os
 # Create your models here.
 
 class Movie(models.Model):
-    srt = models.FileField(blank=True, upload_to="subtitles")
+    srt = models.FileField(blank=True, upload_to=lambda i,filename: f"subtitles/{i.name}.{filename.split('.')[-1]}")
     movie = models.FileField(blank=True, upload_to=lambda i,filename: f"movies/{i.name}.{filename.split('.')[-1]}")
     name = models.CharField(max_length=100, blank=True)
     enabled = models.BooleanField(default=True)
